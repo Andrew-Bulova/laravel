6981c2368d01a86a7808028d503c68c513393ffd
Andrew-Bulova <ghostrider2107@gmail.com>
2020-01-10 22:47:16 +0200

    Начало

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function (){
+    view('layouts.createFakeUserProfile');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.createFakeUserProfile');
 });
